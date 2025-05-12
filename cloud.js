18265d1fd275004807ed83f1f969dccf1c92d11b
The_Impossibles <hirohattsu@gmail.com>
2025-05-12 10:43:08 +0900

    Create cloud.js

---
+++ cloud.js
@@ -0,0 +1,13 @@
+const ws = new Websoket("wss://clouddate.scratch.mit.edu");
+ws.onopen = () => {
+  ws.send(JSONstringify({"method":"handshake","user":"The_Impossibles-sys","project_id":""}) + "\n");
+  console.log("start");
+  ws.send(JSONstringify({"method":"handshake","user":"The_Impossibles-sys","project_id":"","name":"☁︎Test by Js","value":"1234567890"}) + "\n");
+  console.log("finish");
+}
+ws.onclose = () => {
+  console.log("close cloud date")
+}
+  
+
+  
