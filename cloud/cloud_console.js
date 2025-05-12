9d8a2f2d52f9dbf3b679c6fda3215506dc8cc99b
The_Impossibles <hirohattsu@gmail.com>
2025-05-12 21:17:19 +0900

    Update cloud_console.js

--- cloud/cloud_console.js
+++ cloud/cloud_console.js
@@ -1,4 +1,4 @@
-const ws = new WebSocket("wss://clouddate.scratch.mit");
+const ws = new WebSocket("wss://clouddata.scratch.mit");
 
 ws.onopen = () => {
   const username = prompt("Input Your Username");
