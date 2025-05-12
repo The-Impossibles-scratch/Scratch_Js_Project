4b2e5be1c59442c0fc6a615fb5c138abdaef9c15
The_Impossibles <hirohattsu@gmail.com>
2025-05-12 20:59:33 +0900

    Update cloud_console.js

--- cloud/cloud_console.js
+++ cloud/cloud_console.js
@@ -3,11 +3,11 @@ const ws = new WebSocket("wss://clouddate.scratch.mit.edu");
 ws.onopen = () => {
   const user = prompt("Input Your User Name");
   const project_id = prompt("Input Project Id");
-  ws.send(JSONstringify({"method":"handshake","user":user,"project_id":project_id}) + "\n");
+  ws.send(JSON.stringify({"method":"handshake","user":user,"project_id":project_id}) + "\n");
   const cloud_var_name = prompt("Input Cloud Var Name");
   const cloud_value = prompt("Input Cloud Value");
   console.log("start");
-  ws.send(JSONstringify({"method":"handshake","user":user,"project_id":project_id,"name":"☁︎" + cloud_var_name,"value":cloud_value}) + "\n");
+  ws.send(JSON.stringify({"method":"handshake","user":user,"project_id":project_id,"name":"☁︎" + cloud_var_name,"value":cloud_value}) + "\n");
   console.log("finish");
   ws.onclose();
 }
