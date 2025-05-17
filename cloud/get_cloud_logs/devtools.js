97138d68d5887eb2cf365391152cdbd108074155
The_Impossibles <hirohattsu@gmail.com>
2025-05-17 16:50:27 +0900

    Update devtools.js

--- cloud/get_cloud_logs/devtools.js
+++ cloud/get_cloud_logs/devtools.js
@@ -1,1 +1,43 @@
+async function get_cloud_logs(project_id,limit=100,offset=0) {
+  const url = "https://clouddata.scratch.mit.edu/logs?projectid=${project_id}&limit=${limit}&offset=${offset}";
+  
+  try {
+    const fetch = await fetch(url);
+    const cloud_logs = await fetch.json();
+    
+    if (cloud_logs.length == 0) {
+      console.log("There seems to be no Cloud Data activity on this project…")
+      return [];
+    };
+    
+    for (const cloud of cloud_logs) {
+      const output =
+        `
+        ----------------------------------------
+        User: ${cloud.user}
+        Data Name: ${cloud.dataname}
+        Action: ${cloud.verb}
+        Data Value: ${cloud.value}
+        Time: ${new Date(cloud.timestamp).toLocaleString()}
+        ----------------------------------------
+        `;
+      return output;
+    };
+  } catch (error) {
+    console.log("Fetch Error : ",error);
+    return [];
+  };
+}
 
+let project_id = prompt("Input Project ID (If nothing is entered, it will automatically be the current Project Id.)");
+if (!project_id) {
+  const url = window.location.href;
+  const projectId = url.match(/projects\/(\d+)/)?.[1];
+  project_id = Number(projectId);
+}
+
+let limit = prompt("Input Limit (If nothing is entered, it will automatically be 0.)");
+
+let offset = prompt("Input Offset (If nothing is enterd, it will automatically be 0).");
+
+get_cloud_logs(project_id,limit,offset);
