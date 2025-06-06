b922546144d35b2ca59e612767f9b34a65216cc9
The_Impossibles <hirohattsu@gmail.com>
2025-06-06 23:51:52 +0900

    Update devtools.js

--- Other/find user by user id/devtools.js
+++ Other/find user by user id/devtools.js
@@ -9,6 +9,7 @@
   
   function get_csrf_token() {
     const cookies = document.cookie.split(' ;').map(c => c.split('='));
+    
     for ([key, val] of cookies) {
       if (key === "scratchcsrftoken") return val
     }
@@ -17,21 +18,26 @@
 
   async function get_username(csrf_token, headers) {
     let return_val;
+    
     if (!csrf_token) {
       throw new Error("CSRF Token Is Not Found");
     };
+    
     let headers_copy = {...headers, "X-CSRFToken":csrf_token};
+    
     try {
       const fetch_res = await fetch('https://scratch.mit.edu/session', {
         method: "POST",
         headers: headers_copy,
         credentials: "include"
       })
+      
       return_val = await fetch_res.json().user?.username
       if (!return_val) {
         throw new Error("you must be log in");
       };
       return return_val;
+      
     } catch (e) {
       throw new Error("ERROR : " + e.message);
     };
@@ -39,6 +45,7 @@
 
   async function del_comment(username, comment_id, headers, csrf_token) {
     let headers_copy = {...headers, "X-CSRFToken":csrf_token};
+    
     try {
       const fetch_res = await fetch(`https://scratch.mit.edu/site-api/comments/user/${username}/del/`, {
         method: "POST",
@@ -46,6 +53,7 @@
         credentials: "include",
         body: JSON.stringify({id: String(comment_id)})
       });
+      
     } catch (e) {
       throw new Error("ERROR : " + e.message);
     };
@@ -53,6 +61,7 @@
 
   async function post_comment(username, commentee_id, headers, csrf_token) {
     let headers_copy = {...headers, "X-CSRFToken":csrf_token};
+    
     try {
       const fetch_res = await fetch(`https://scratch.mit.edu/site-api/comments/user/${username}/add/`, {
         method: "POST",
@@ -64,6 +73,9 @@
           commentee_id: commentee_id
         })
       });
+      
+      let html = fetch_res.text()
+      
     } catch (e) {
       throw new Error("ERROR : " + e.message);
     };
